8b541a21cc11e50d99cf1fac832d6079e9810ead
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-02-21 10:54:49 +0100

    feat(model): added hover interaction

--- components/gl/Scene.jsx
+++ components/gl/Scene.jsx
@@ -1,11 +1,15 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, Environment, Loader } from '@react-three/drei';
 
 import { useControls } from 'leva';
 
-import { Suspense } from 'react';
+import { Suspense, useRef, useState } from 'react';
 
-export default function Scene() {
+function Model() {
+  const meshRef = useRef();
+
+  const [hovered, hover] = useState(false);
+
   const { color } = useControls({
     color: {
       value: '#FF69B4',
@@ -13,14 +17,35 @@ export default function Scene() {
       format: (v) => v,
     },
   });
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
+
+  if (meshRef.current) console.log(meshRef.current);
+
+  return (
+    <mesh
+      ref={meshRef}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+      scale={hovered ? 2 : 1}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={color} roughness={0} />
+    </mesh>
+  );
+}
+
+export default function Scene() {
   return (
     <>
       <Canvas shadows>
         <Suspense fallback={null}>
-          <mesh>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={color} />
-          </mesh>
+          <Model />
           <OrbitControls />
           <ambientLight intensity={1} />
           <Environment preset="city" />
